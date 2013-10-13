3fe21186574d054f5995d78201f2c3a8ed19f472
Will Newman <wnewman88@gmail.com>
2013-10-13 02:32:30 -0400

    SadBoyz
    mostly sort of working...

--- flickr_feed.php
+++ flickr_feed.php
@@ -1,22 +1,18 @@
 <?php
-include "phpFlickr.php";
-$tag1 = "wtf";
-$tag2 = "jody highroller";
-$tag3 = "sadboyz";
-$imgsArray = array(flickr_feed($tag1));
+function randomFlickrImage($tag = "jody highroller"){
+	include "inc/phpFlickr.php";
+	$f = new phpFlickr("c329e648c58a21803f2dda3af61bd17e", "222b02b98b03cf25");
+	
+	$tagArray = $f->photos_search(array("tags"=>$tag, "tag_mode"=>"any","per_page"=>"20"));
+	$imageUrls = array();
+	
+	foreach($tagArray['photo'] as $photo){
+		array_push( $imageUrls, $f->buildPhotoURL($photo, 'medium') );
+	}
+	$rand = mt_rand(0, count($imageUrls) -1 );
 
-flickr_feed($tag2);
-flickr_feed($tag3);
-
-
-function flickr_feed ( $tag ) {
-    $f = new phpFlickr("c329e648c58a21803f2dda3af61bd17e", "222b02b98b03cf25");
-    $tags = $f->photos_search(array("tags"=>$tag, "tag_mode"=>"any","per_page"=>"20"));
-    return $tags;
-    //foreach ($tags['photo'] as $photo) {
-    	//$first = " first";
-    	//echo '<div><img src="'. $f->buildPhotoURL($photo, 'medium') .'" alt="" /></div>';
-     }
-    			
-
+	return $imageUrls[$rand];
+}
 ?>
+
+<img src="<?php echo randomFlickrImage("twerk");?>">
