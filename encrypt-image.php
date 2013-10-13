a5132886ac2dfecc4cce55b05da783a30fffc8f3
g-szabo <gabrielszabo@nbp-93-76.nbp.ruw.rutgers.edu>
2013-10-13 08:39:37 -0400

    2 space padding between key and timestamp

--- encrypt-image.php
+++ encrypt-image.php
@@ -34,7 +34,7 @@
 	$sid = "AC2f9c27f3c7092d24ad88b5377f546bec";
 	$token = "9c15613bef48f0085ae7a106fe547b43";
 	$client = new Services_Twilio($sid, $token);
-	$message = "Encryption Key: %'x1s Timestamp: %$s";
+	$message = "Encryption Key: %' 2s Timestamp: %$s";
 	$recipientNumber = $messageInfo['phoneNumber'];
 
 	$client->account->messages->sendMessage("+15512266955", $recipientNumber, sprintf($message, $randomKey, $filename);
