bbc74e84c7b288f0b0e5fe394d4d646617f7d80d
g-szabo <gabrielszabo@nbp-93-76.nbp.ruw.rutgers.edu>
2013-10-13 08:22:03 -0400

    yet another format attempt

--- encrypt-image.php
+++ encrypt-image.php
@@ -34,9 +34,9 @@
 	$sid = "AC2f9c27f3c7092d24ad88b5377f546bec";
 	$token = "9c15613bef48f0085ae7a106fe547b43";
 	$client = new Services_Twilio($sid, $token);
-	$message = "Encryption Key: ".$randomKey ."\n Timestamp: ".$filename;
+	$message = "Encryption Key: %$s Timestamp: %$s";
 	$recipientNumber = $messageInfo['phoneNumber'];
 
-	$client->account->messages->sendMessage("+15512266955", $recipientNumber, sprintf($message);
+	$client->account->messages->sendMessage("+15512266955", $recipientNumber, sprintf($message, $randomKey, $filename);
 
 ?>
